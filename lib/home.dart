eb344ed38ef921d2005af3ebe115c42cf20ea485
GosuCode <gosucode1945@gmail.com>
2023-12-18 22:08:56 +0545

    placeholder

--- lib/home.dart
+++ lib/home.dart
@@ -1,42 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double deviceHeight = MediaQuery.of(context).size.height;
-    double deviceWidth = MediaQuery.of(context).size.width;
+    // double deviceHeight = MediaQuery.of(context).size.height;
+    // double deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      backgroundColor: Color.fromARGB(255, 238, 247, 227),
+      body: Column(
         children: [
-          Container(
-            child: Text('Best Plants For\n Our Green House',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 40, left: 20),
+                child: const Text('Best Plants For\n Our Green House',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 40, right: 20),
+                child: const CircleAvatar(
+                  radius: 35,
+                ),
+              ),
+            ],
           ),
-          Container(
-            child: CircleAvatar(
-              radius: 50,
-            ),
-          ),
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
-
-      // Text(
-      //   'Best Plants For Our Green House',
-      //   style: TextStyle(
-      //     color: Colors.black,
-      //     fontSize: 30,
-      //     fontWeight: FontWeight.bold,
-      //   ),
-      // ),
     );
   }
 }
